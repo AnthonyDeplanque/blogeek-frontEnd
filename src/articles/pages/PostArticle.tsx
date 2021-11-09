b6ce798d86834c83e9aee205045c3581df6e848f
anthony Deplanque <anthony.deplanque3184@gmail.com>
2021-11-09 18:21:46 +0100

    refacto(article):better visual for article form

--- src/articles/pages/PostArticle.tsx
+++ src/articles/pages/PostArticle.tsx
@@ -24,13 +24,28 @@ const PostArticle: React.FC<PostArticleProps> = (props) => {
   return (
     auth?.roles?.includes(ROLE.ROLE_CREATOR) || auth?.roles?.includes(ROLE.ROLE_ADMIN) ? (
       <MainCard title="Poster un article" display="flex" flexDirection="row" alignItems="center" justifyContent="stretch">
-        <Box width="100%" padding={theme.spacing(2)}><TextField style={{ width: "100%" }} name="title" onChange={(e) => setTitle(e.target.value)}>
-        </TextField></Box>
-        <Box width="100%" padding={theme.spacing(2)}><TextField style={{ width: "100%" }} name="subtitle" onChange={(e) => setSubtitle(e.target.value)}>
-        </TextField></Box>
-        <Box width="100%" padding={theme.spacing(2)}><TextField style={{ width: "100%" }} name="content" onChange={(e) => setContent(e.target.value)}>
-        </TextField></Box>
-        <Button variant="contained" onClick={handlePostArticle}>Poster l'article</Button>
+        <Box width="100%" padding={theme.spacing(2)}>
+          <TextField label="titre" style={{ width: "100%" }} name="title" onChange={(e) => setTitle(e.target.value)}>
+          </TextField>
+        </Box>
+
+        <Box width="100%" padding={theme.spacing(2)}>
+          <TextField label="sous titre" style={{ width: "100%" }} name="subtitle" onChange={(e) => setSubtitle(e.target.value)}>
+          </TextField>
+        </Box>
+
+        <Box width="100%" padding={theme.spacing(2)}>
+          <TextField
+            label="contenu"
+            multiline
+            rows={8}
+            style={{ width: "100%" }}
+            name="content"
+            onChange={(e) => setContent(e.target.value)}>
+          </TextField>
+        </Box>
+
+        <Box width="100%" display="flex" flexDirection="row" justifyContent="center"><Button variant="contained" onClick={handlePostArticle}>Poster l'article</Button></Box>
       </MainCard>)
       : (<Typography>Vous devez être un créateur de contenu pour pouvoir poster un article</Typography>))
 }
